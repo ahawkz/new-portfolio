8618be1f480f2b30c3a7eec59dddcddba2e3dc80
Alex <ahawknj15@gmail.com>
2022-02-05 10:13:59 -0500

    updated homepage and button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,20 +6,24 @@ const STYLES = ['btn--primary', 'btn--outline'];
 
 const SIZES = ['btn--medium', 'btn--large'];
 
-export const Button = ({children, type, onClick, buttonStyles, buttonSize}) => {
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyles,
+  buttonSize
+}) => {
   const checkButtonStyle = STYLES.includes(buttonStyles) ? buttonStyles : STYLES[0];
 
-  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
-    <Link to='/sign-up' className='btn-mobile'>
-      <button
-        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-        onClick={onClick}
-        type={type}
-      >
-        {children}
-      </button>
-    </Link>
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
   )
 };
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css'
@@ -6,38 +7,27 @@ import './HeroSection.css'
 function HeroSection() {
   return (
     <div className='home-background'>
-      <div className='gray-bg'></div>
+      <div className='color-bg'></div>
       <div className='hero-container'>
         <div className='hero-left'>
           <p className='overline'>hi there</p>
           <h1>I'm Alex, a Frontend Developer based out of West Chester, PA.</h1>
           <div className='hero-btns'>
-            <Button
-              className='btns'
-              buttonStyle='btn--outline'
-              buttonSize='btn--large'
-            >
-              About Me
-            </Button>
-            <Button
-              className='btns'
-              buttonStyle='btn--primary'
-              buttonSize='btn--large'
-              onClick={console.log('hey')}
-            >
-              Say Hello
-            </Button>
+          <Link to="/about">
+            <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>About Me</Button>
+          </Link>
+          <Link to="/contact">
+            <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Say Hello</Button>
+          </Link>
           </div>
         </div>
         <div className='hero-right'>
           <p className='overline'>MY WORK</p>
-          <p className='text-large'>Lorem
+          <p className='text-large'>Links
           </p>
-          <p className='text-large'>Lorem
+          <p className='text-large'>Coming
           </p>
-          <p className='text-large'>Lorem
-          </p>
-          <p className='text-large'>Lorem
+          <p className='text-large'>Soon
           </p>
         </div>
       </div>
